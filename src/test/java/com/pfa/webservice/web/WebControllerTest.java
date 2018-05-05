30c709e5fa52da798a54bd773eff3b26ac5af44a
DESKTOP-5533OO2\jhleon <jhleon869@gmail.com>
2018-05-05 17:47:06 +0900

    no message

--- src/test/java/com/pfa/webservice/web/WebControllerTest.java
+++ src/test/java/com/pfa/webservice/web/WebControllerTest.java
@@ -29,7 +29,7 @@ public class WebControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("aaaaaaaa");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 
 }
