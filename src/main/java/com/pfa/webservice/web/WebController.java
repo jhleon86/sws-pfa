16b38b5f328464599f2d5f2ab23b6ce666d89973
DESKTOP-5533OO2\jhleon <jhleon869@gmail.com>
2018-05-10 15:34:57 +0900

    no message

--- src/main/java/com/pfa/webservice/web/WebController.java
+++ src/main/java/com/pfa/webservice/web/WebController.java
@@ -1,8 +1,8 @@
 package com.pfa.webservice.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.pfa.webservice.service.PostsService;
 
@@ -20,19 +20,20 @@ public class WebController {
 	
 	private PostsService postsService;
 	
-	/*
+	
     @GetMapping("/")
     public String main(Model model) { 
     	model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
-    */
 	
+    /*
 	@GetMapping("/")
     public ModelAndView main() { 
 		ModelAndView mav = new ModelAndView("main") ;
 		mav.addObject("posts", postsService.findAllDesc());
         return mav;
     }
+    */
 	
 }
